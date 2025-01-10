f88b5c02375d88b33e4a7e3a729ee0ea80302e50
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-01-10 22:46:06 +0100

    working on coffee tile

--- lib/utilities/coffee_tile.dart
+++ lib/utilities/coffee_tile.dart
@@ -8,10 +8,37 @@ class CoffeeTile extends StatelessWidget {
     return (Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
+        padding: const EdgeInsets.all(12),
         width: 200,
         decoration: BoxDecoration(
             color: Colors.black54, borderRadius: BorderRadius.circular(12)),
-        child: Image.asset('assets/images/image1.jpg'),
+        child: Column(children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset('assets/images/image1.jpg'),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 12.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Latte',
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'With Almond Milk',
+                  style: TextStyle(color: Colors.grey[700]),
+                )
+              ],
+            ),
+          )
+        ]),
       ),
     ));
   }
