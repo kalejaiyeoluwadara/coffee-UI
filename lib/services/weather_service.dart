a31eaf7cbb6b60b9bd4738ae377cff64f912870b
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-01-11 20:48:48 +0100

    yyyy

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,10 +6,12 @@ import 'package:http/http.dart' as http;
 class WeatherService {
   final String apiKey;
   final String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
   WeatherService(this.apiKey);
+
   Future<Map<String, dynamic>> getWeather(String city) async {
-    final response =
-        await http.get(Uri.parse('$baseUrl?q=$city&appid=$apiKey'));
+    final response = await http
+        .get(Uri.parse('$baseUrl?q=$city&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
       return json.decode(response.body);
@@ -20,6 +22,7 @@ class WeatherService {
 
   Future<String> getCurrentCity() async {
     LocationPermission permission = await Geolocator.checkPermission();
+
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
@@ -31,11 +34,18 @@ class WeatherService {
       throw Exception('Location permissions are permanently denied');
     }
 
+    // Get the current position
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
+
+    // Reverse geocoding to get the city name
     List<Placemark> placemarks =
         await placemarkFromCoordinates(position.latitude, position.longitude);
-    String? city = placemarks[0].locality;
-    return city ?? "";
+    if (placemarks.isNotEmpty) {
+      String? city = placemarks[0].locality;
+      return city ?? "Unknown city";
+    } else {
+      throw Exception('Failed to retrieve the city name');
+    }
   }
 }
